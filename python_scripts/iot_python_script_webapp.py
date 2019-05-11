78db5737634720446ea12f7848b26c4f41ace383
Jessica Huynh <jessicahuynh@dyn-160-39-130-151.dyn.columbia.edu>
2019-05-11 17:00:49 -0400

    color change, scability and json file testing

--- python_scripts/iot_python_script_webapp.py
+++ python_scripts/iot_python_script_webapp.py
@@ -7,17 +7,20 @@ import fileinput
 
 app = Flask(__name__)
 data = {}
-cleaned_port_numbers = ['4000']
+port_numbers = []
+actuators = []
 
 @app.route('/', methods=['GET'])
 def node_1_get():
-    # gets port numbers and cleans them up
-    # temp = requests.get(url="http://128.59.22.210:4000/").json()
-    # port_numbers = temp["port_numbers"]
-    # cleaned_port_numbers = []
-    # for port_number in port_numbers:
-    #     if port_number != '':
-    #         cleaned_port_numbers.append(port_number)
+    # reads in config JSON file
+    with open('sample_config.json') as json_file:
+        data = json.load(json_file)
+        for key in data.keys():
+            current_actuators = []
+            if "sw" in key:
+                port_numbers.append(str(data[key]['port']))
+                current_actuators = list(map(str, data[key]['outgoing']))
+                actuators.append(current_actuators.copy())
     
     html = None
     if os.stat('templates/node_1.html').st_size == 0:
@@ -35,10 +38,11 @@ def node_1_get():
           <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
           <link rel="stylesheet" href="../static/node_1.css">
         </head>\n""")
-    for port in cleaned_port_numbers:
-        html.write("""        <div><input type="checkbox" class='toggle' id='box""" + port + """' checked data-toggle="toggle"></div>\n""")
-        html.write("""        <div class='status' id='toggledbox""" + port + """'>On</div>\n""")
-    html.write("""    <div class="blue", id="circle0"></div>
+    for i in range(len(port_numbers)):
+        html.write("""        <div><input type="checkbox" class='toggle' id='box""" + port_numbers[i] + """' checked data-toggle="toggle"></div>\n""")
+        for j in range(len(actuators[i])):
+            html.write("""        <div class="red", id="circle""" + actuators[i][j] + """" style="margin-bottom:20px;"></div>\n""")
+    html.write("""
     </body>
     <script>
     $(document).ready(function() {
@@ -50,15 +54,20 @@ def node_1_get():
             success: function(response) {
                 console.log(response);
                 response = JSON.parse(response);
-                console.log(response);
-                if (Object.keys(response).indexOf("actuator1") >= 0){
-                    if (response["actuator1"] == 1) {
-                        $("#circle0").attr("class", "black");
+                console.log(response);""")
+    for i in range(len(port_numbers)):
+        for j in range(len(actuators[i])):
+            html.write("""
+                if (Object.keys(response).indexOf("a""" + actuators[i][j] + """") >= 0){
+                    if (response["a""" + actuators[i][j] + """"] == 1) {
+                        $("#circle""" + actuators[i][j] + """").attr("class", "black");
                     }
                     else {
-                        $("#circle0").attr("class", "blue");
+                        $("#circle""" + actuators[i][j] + """").attr("class", "red");
                     }
                 }
+            """)
+    html.write("""
             },
             complete: function(response) {
                 setTimeout(get_status, interval);
@@ -66,19 +75,13 @@ def node_1_get():
         });
         }
         setTimeout(get_status, interval);\n""")
-    for port in cleaned_port_numbers:
-        html.write("""        $('#box""" + port + """').change(function() {
-          var status = document.getElementById('toggledbox""" + port + """').innerHTML
-          if (status == 'On') {
-            $('#toggledbox""" + port + """').html('Off');
-          }
-          else {
-            $('#toggledbox""" + port + """').html('On');
-          }
+    for i in range(len(port_numbers)):
+        html.write("""        $('#box""" + port_numbers[i] + """').change(function() {
+          var status = $('#box""" + port_numbers[i] + """').prop("checked");
           $.ajax({
                 url: "/get_toggled_status",
                 type: "GET",
-                data: {status: status, port: """ + port + """},
+                data: {status: status, port: """ + port_numbers[i] + """},
             });
         });\n""")
     html.write("""      });
@@ -100,22 +103,11 @@ def toggled_status():
     current_status = request.args.get('status')
     port_number = request.args.get('port')
 
-    if current_status == 'On':
+    if current_status == 'true':
         # current testing
-        result = requests.post(url="http://128.59.22.210:4000/", data={'actuator1': 1})
-
-        # for the future
-        # sending_data = data["open_whisk" + str(port_number)]
-        # # container
-        # connected = False
-        # # need to check which URLs are open
-        # while not connected:
-        #     result = requests.post(url="http://128.59.22.210:80/", json=sending_data)
-        #     sleep(1)
-        #     if result.status_code == requests.codes.ok:
-        #         connected = True
+        result = requests.post(url="http://128.59.22.210:4000/", data={'a5002': 1})
     else:
-        result = requests.post(url="http://128.59.22.210:4000/", data={'actuator1': 0})
+        result = requests.post(url="http://128.59.22.210:4000/", data={'a5002': 0})
 
     return render_template('node_1.html')
 
