962a951c5fadbfefff95d5ad44f63fad84688972
Jessica Huynh <jessicahuynh@Jessicas-MacBook-Air-3.local>
2019-05-23 14:52:31 -0400

    changed name of container

--- shell/run_api.sh
+++ shell/run_api.sh
@@ -1,4 +1,4 @@
 docker build -t db:latest -f database/Dockerfile .
 docker built -t api:latest -f api/Dockerfile .
 docker run -d -p 4001:4001 api
-docker run -d -p 4000:4000 db
+docker run -d -p 4000:4000 database
